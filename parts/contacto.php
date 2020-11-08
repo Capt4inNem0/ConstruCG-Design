884335f890bc4b8c257a5158d9b46f496f0bdff7
Capt4inNem0 <gastoncollo@gmail.com>
2020-11-08 20:01:06 -0300

    Añadido estilos de contacto

--- parts/contacto.php
+++ parts/contacto.php
@@ -1,18 +1,20 @@
 <article id="contacto">
-    <h2>Contacto</h2>
-    <form action="" method="post">
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre">
+    <div class="mr-auto">
+        <h2>Contacto</h2>
+        <form action="" method="post">
+            <label for="nombre">Nombre</label>
+            <label for="apellido">Apellido</label>
 
-        <label for="apellido">Apellido</label>
-        <input type="text" name="apellido">
+            <input type="text" name="nombre">        
+            <input type="text" name="apellido">
 
-        <label for="email">E-mail</label>
-        <input type="email" name="email" id="">
+            <label for="email" class="col-1-2">E-mail</label>
+            <input type="email" name="email" id="" class="col-1-2">
 
-        <label for="consulta">Consulta:</label>
-        <textarea name="consulta" id="" cols="30" rows="10"></textarea>
+            <label for="consulta" class="col-1-2">Consulta:</label>
+            <textarea name="consulta" id="" cols="30" rows="10" class="col-1-2"></textarea>
 
-        <input type="submit" value="ENVIAR">
-    </form>
+            <input type="submit" value="ENVIAR" class="article-button">
+        </form>
+    </div>
 </article>
