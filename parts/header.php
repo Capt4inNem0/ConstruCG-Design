d4442af63793fa7b007f600089aaaaca048f4e34
reckxail <68757654+reckxail@users.noreply.github.com>
2020-11-08 20:26:10 -0300

    de Trabajo a trabajo

--- parts/header.php
+++ parts/header.php
@@ -1,6 +1,10 @@
 
 <header class="header2">
+<<<<<<< HEAD
 	<img class="logo2" src="/htdocs/imagenes/logo.png">
+=======
+	<img class="logo2" src="/imagenes/logo.png">
+>>>>>>> e4588eb2d3c442ea70f7a9706340cf2c8dfb7ea5
   <div class="wrapper">
     <div class="logo"> ConstruCG </div>
       <nav >
