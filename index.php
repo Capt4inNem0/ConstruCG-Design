585d7bd92e9bbdefe72d05433b720f77e1fb3a24
Capt4inNem0 <gastoncollo@gmail.com>
2020-11-08 15:16:33 -0300

    Añadida primera pagina

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ConstruCG</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/parts/inicio.css">
 </head>
     <body>
-        
+        <?php
+            include __DIR__."/parts/inicio.php"
+        ?>
     </body>
 </html>
--- parts/inicio.php
+++ parts/inicio.php
@@ -1,3 +1,9 @@
-<article>
-    
+<article id="nosotros">
+    <div class="carousel">
+        <h1>carrousel de imagenes</h1>
+    </div>
+    <div class="nosotros">
+        <h1>Nosotros</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, esse ducimus consequuntur quo laboriosam labore fuga. Delectus aspernatur eius, id numquam enim, unde iste nostrum, beatae tenetur similique voluptatem fugit.</p>
+    </div>
 </article>
