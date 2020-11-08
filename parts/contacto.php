41201b4569c9cb022fe6b204e2d1524d4e4b7db6
Capt4inNem0 <gastoncollo@gmail.com>
2020-11-08 19:15:11 -0300

    Añadida pagina de contacto

---
+++ parts/contacto.php
@@ -0,0 +1,18 @@
+<article id="contacto">
+    <h2>Contacto</h2>
+    <form action="" method="post">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre">
+
+        <label for="apellido">Apellido</label>
+        <input type="text" name="apellido">
+
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="">
+
+        <label for="consulta">Consulta:</label>
+        <textarea name="consulta" id="" cols="30" rows="10"></textarea>
+
+        <input type="submit" value="ENVIAR">
+    </form>
+</article>
