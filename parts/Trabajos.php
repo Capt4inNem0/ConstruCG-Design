7bc438f04d1fdb25e5e0f9e12c0c9f6e454828c6
reckxail <68757654+reckxail@users.noreply.github.com>
2020-11-08 21:03:03 -0300

    img

--- parts/Trabajos.php
+++ parts/Trabajos.php
@@ -1,13 +1,13 @@
 <article id="trabajo">
   <div class="trabajo2">
 
-  	
+
 	<h2>Trabajos Realizados</h2>
 	
 
 <div class="item" > 
 
-     <img class="pcweb" src="imagenes/pcweb.png">
+     <img class="pcweb" src=/imagenes/logo.png">
      <br/><br/><br/>
      <p class="paginas">Hola mundo</p>
 </div>
