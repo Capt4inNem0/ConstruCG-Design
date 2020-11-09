cf175dfeda424fc1a40cd095953e94f25abe46a6
reckxail <68757654+reckxail@users.noreply.github.com>
2020-11-09 15:29:09 -0300

    trabajo

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="/css/parts/inicio.css">
     <link rel="stylesheet" href="/css/parts/trabajos.css">
     <link rel="stylesheet" href="/css/parts/grid.css">
+
     <link rel="stylesheet" href="/css/alarma.css">
 
     <link rel="stylesheet" href="css/parts/header.css">
--- parts/Trabajos.php
+++ parts/Trabajos.php
@@ -4,14 +4,14 @@
 
 	<h2>Trabajos Realizados</h2>
 
-   <div class="contenedor-trabajo">
+   <div class="contenedor12">
    	<div>1</div>
     <div>2</div> 
     <div>3</div>
     <div>4</div>
     <div>5</div>
     <div>6</div>
-    <div>7</div>
+
 
    </div>
 
