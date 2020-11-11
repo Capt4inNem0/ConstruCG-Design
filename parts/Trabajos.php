de2625e1333a3406a5bbf31e6743666f11d04f08
reckxail <68757654+reckxail@users.noreply.github.com>
2020-11-11 00:53:05 -0300

    trabajo

--- parts/Trabajos.php
+++ parts/Trabajos.php
@@ -1,17 +1,30 @@
 <article id="trabajo">
-  <div class="centrado">
 
 
-	<h2>Trabajos Realizados</h2>
 
-   <div class="contenedor12">
-   	<div><img src="/htdocs/imagenes/carousel/1.png"></div>
-    <div> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, esse ducimus consequuntur quo laboriosam labore fuga.</p></div> 
-    <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, esse ducimus consequuntur quo laboriosam labore fuga.</p></div>
-    <div><img src="/htdocs/imagenes/carousel/1.png"></div>
-    <div><img src="/htdocs/imagenes/carousel/1.png"></div>
-    <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, esse ducimus consequuntur quo laboriosam labore fuga.</p></div>
 
+
+  <div class="contenedor2">
+<div class = "header">
+    <p> Paginas Webs </p>
+  </div>
+
+  <div class = "contenedor">
+    <div> <img src="imagenes/pcweb.png">  </div>
+    <div> <img src="imagenes/pcweb.png">  </div>
+    <div> <img src="imagenes/pcweb.png">  </div>
+    <div> <h3>PRESENCIA</h3> </div>
+    <div> <h3>NEGOCIO</h3> </div>
+    <div> <h3>TIENDA ONLINE</h3>  </div>
+    <div> <p>Consigue una presencia profesional en Internet y genera nuevos contactos.</p> </div>
+    <div> <p>Lleva tu empresa a nuevos mercados con un sitio web completo y eficaz.</p>  </div>
+    <div> <p>Multiplica tus ventas con una tienda online y llega a más clientes.</p> </div>
+    <div> <h5>Precio Unico Anual </br>  $4.490 </br> Pesos Argentinos </h5> <p></br> Tu nombre en Internet Diseño de 1 sitio con 1 página 1 cuenta de correo</p> </div>
+
+    <div> <h5>Precio Unico Anual </br>$9.578</br> Pesos Argentinos</h5> <p>     </br>Tu nombre en Internet Diseño de 1 sitio con 5 páginas 5 cuentas de correo</p> </div>
+
+    <div> <h5>Precio Unico Anual </br> $15.490 </br> Pesos Argentinos </h5><p></br>< Tu nombre en Internet info Diseño de 1 sitio con tienda online 20 cuentas de correo </p> </div>
+  </div>
 
    </div>
 </div>
